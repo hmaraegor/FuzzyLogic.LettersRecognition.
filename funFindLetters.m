e8d081177626fbbaaca466432fd648fd0fc455f1
hmaraegor <hmaraegor@gmail.com>
2017-10-27 20:27:52 +0300

    Update funFindLetters.m

--- funFindLetters.m
+++ funFindLetters.m
@@ -1,30 +1,36 @@
-function Ans = funFindLetters(A, Start, Final)
-Left=0; Right=length(A(1,:))+1;
-Top=length(A(:,1))+1; Bottom=0;
-Ans=zeros(1,10);
-STR=1;
-    for j=1:1:length(A(1,:)) %кол-во j
-        f=0;
-        for i=Start:1:Final %–ассматриваем только присланную строку
-            if A(i,j)~=1 & Left==0  %≈сли пиксель не белый, то
-                Left=j;             %это лева€ граница буквы
+% This function find top, bottom and edges each letter.
+
+function Answer = funFindLetters(BlackWhiteImage, TopStr, BottomStr)
+LeftLett = 0; 
+RightLett = length(BlackWhiteImage(1,:))+1;
+TopLett = length(BlackWhiteImage(:,1))+1; 
+BottomLett = 0;
+Answer = zeros(1,10);
+StringNumber = 1;
+    for j = 1:1:length(BlackWhiteImage(1,:))
+        BotL = 0;
+        for i = TopStr:1:BottomStr
+            if BlackWhiteImage(i,j)~=1 & LeftLett==0
+                LeftLett = j;
             end
-            if A(i,j)~=1 & Top>i    %≈сли пиксель нижеt верхней границы не белый,
-               Top=i;               %то попровл€ем границу буквы 
+            if BlackWhiteImage(i,j)~=1 & TopLett>i
+               TopLett = i;
             end
-            if A(i,j)~=1 & Bottom<i %≈сли пиксель выше нижней границы не белый,
-               Bottom=i;            %то попровл€ем нижнюю границу буквы 
+            if BlackWhiteImage(i,j)~=1 & BottomLett<i
+               BottomLett = i;
             end
-            if A(i,j)~=1            %≈сли еще не конец буквы, то
-               f=1;                 %указываем это
+            if BlackWhiteImage(i,j)~=1
+               BotL = 1;
             end
         end
-        if f==0 & Left~=0
-           Right=j-1;
-           Ans(STR,:)=[funLetterProcessing(A,Left,Right,Top,Bottom) STR];
-           STR=STR+1;
-           Left=0; Right=length(A(1,:))+1;
-           Top=length(A(:,1))+1; Bottom=0;
+        if BotL==0 & LeftLett~=0
+           RightLett = j - 1;
+           Answer(StringNumber,:) = [funLetterProcessing(BlackWhiteImage,LeftLett,RightLett,TopLett,BottomLett) StringNumber];
+           StringNumber = StringNumber + 1;
+           LeftLett = 0; 
+           RightLett = length(BlackWhiteImage(1,:)) + 1;
+           TopLett = length(BlackWhiteImage(:,1)) + 1; 
+           BottomLett = 0;
         end
     end
 end
