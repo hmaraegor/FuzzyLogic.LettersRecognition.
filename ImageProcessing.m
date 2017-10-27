f33697322758fd1b730660563d0c2663dda5ce48
hmaraegor <hmaraegor@gmail.com>
2017-10-27 19:35:43 +0300

    Update ImageProcessing.m

--- ImageProcessing.m
+++ ImageProcessing.m
@@ -1,6 +1,6 @@
 % This function start to create learning sample
 
-Image3uint8 = imread('<E:\Documents\LearningSampleImage.bmp>'); %sample with strings
+Image3uint8 = imread('<E:\Documents\LearningSampleImage.bmp>');     %sample with strings
 Image1uint8 = Image3uint8(:,:,1); 
 BlackWhiteImage = roicolor(Image1uint8,250,255);
 Answer = funFindStrings(BlackWhiteImage);     %finding strings
