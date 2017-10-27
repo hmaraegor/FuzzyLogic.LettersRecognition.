e81e603f6a8c9f4435e4d33d493294ec35625104
hmaraegor <hmaraegor@gmail.com>
2017-10-27 20:27:27 +0300

    Update funLetterProcessing.m

--- funLetterProcessing.m
+++ funLetterProcessing.m
@@ -1,56 +1,59 @@
-function Ans = funLetterProcessing(A, Left, Right, Top, Bottom)
-    J=zeros(Bottom-Top,Right-Left);
-    for i=Top:1:Bottom
-            for j=Left:1:Right
-                J(i-Top+1,j-Left+1)=A(i,j);
+% Processing letter. Splitting the letter on 9 parts (3x3).
+% Each part is the ratio of white pixels to black.
+
+function Answer = funLetterProcessing(BlackWhiteImage, LeftLett, RightLett, TopLett, BottomLett)
+    Letter = zeros(BottomLett - TopLett, RightLett - LeftLett);
+    for i = TopLett:1:BottomLett
+            for j = LeftLett:1:RightLett
+                Letter(i - TopLett + 1, j - LeftLett + 1) = A(i,j);
             end
     end
-    J=imresize(J, 64, 'bilinear');  % увеличю в 8 раза
-    %figure, imshow(J); title('resize');
-    J=BlackWhite(J);
-    %figure, imshow(J); title('resize');
-    Si=length(J(:,1));
-    Sj=length(J(1,:));
+    Letter=imresize(Letter, 64, 'bilinear');        % resize letter x64
+    Letter=BlackWhite(Letter);
+    LetterHeight=length(Letter(:,1));
+    LetterWidth=length(Letter(1,:));
     
-    Mi=rem(Si, 3);
-    Mj=rem(Sj, 3);
+    HeightRemainder=rem(LetterHeight, 3);
+    WidthRemainder=rem(LetterWidth, 3);
 
-    if Mi==1
-        J(Si+1,:)=1; J(Si+2,:)=1;
+    if HeightRemainder==1
+        Letter(LetterHeight+1,:) = 1; 
+        Letter(LetterHeight+2,:) = 1;
     end
-    if Mi==2
-        J(Si+1,:)=1;
+    if HeightRemainder==2
+        Letter(LetterHeight+1,:) = 1;
     end
-    if Mj==1
-        J(:,Sj+1)=1;   J(:,Sj+2)=1;
+    if WidthRemainder==1
+        Letter(:,LetterWidth+1) = 1;   
+        Letter(:,LetterWidth+2) = 1;
     end
-    if Mj==2
-        J(:,Sj+1)=1;
+    if WidthRemainder==2
+        Letter(:,LetterWidth+1) = 1;
     end    
 
-    Si=length(J(:,1));
-    Sj=length(J(1,:));
+    LetterHeight = length(Letter(:,1));
+    LetterWidth = length(Letter(1,:));
 
-    Ans=zeros(1,9);
+    Answer = zeros(1,9);
     
-    Di=[0 0 0 Si/3 Si/3 Si/3 2*Si/3 2*Si/3 2*Si/3];
-    Dj=[0 Sj/3 2*Sj/3 0 Sj/3 2*Sj/3 0 Sj/3 2*Sj/3];
-    for k=1:1:9
-        Zero=0;
-        One=0;
-        for i=Di(k)+1:1:Di(k)+Si/3
-            for j=Dj(k)+1:1:Dj(k)+Sj/3
-               if J(i,j)==1
-                  Zero=Zero+1;
+    PartI = [0 0 0 LetterHeight/3 LetterHeight/3 LetterHeight/3 2*LetterHeight/3 2*LetterHeight/3 2*LetterHeight/3];
+    PartJ = [0 LetterWidth/3 2*LetterWidth/3 0 LetterWidth/3 2*LetterWidth/3 0 LetterWidth/3 2*LetterWidth/3];
+    for k = 1:1:9
+        Zero = 0;
+        One = 0;
+        for i = PartI(k)+1:1:PartI(k) + LetterHeight/3
+            for j = PartJ(k)+1:1:PartJ(k) + LetterWidth/3
+               if Letter(i,j)==1
+                  Zero = Zero + 1;
                else
-                   One=One+1;
+                   One = One + 1;
                end
             end
         end
         if Zero~=0 & One/Zero<=2
-        Ans(k)=One/Zero; %((One+1)/(Zero+1))/(Bottom-Top);
+        Answer(k) = One/Zero;
         else
-            Ans(k)=2;
+            Answer(k) = 2;
         end
     end
 end
